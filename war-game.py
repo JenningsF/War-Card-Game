6455f7b1d627db177a94786c4140bf993819c182
JenningsF <jenningsf@protonmail.com>
2022-12-03 22:07:21 -0600

    Beginning game logic

--- war-game.py
+++ war-game.py
@@ -50,10 +50,36 @@ class Player:
     def __str__(self) -> str:
         return f"Player {self.name} has {len(self.all_cards)}"
 
-new_deck = Deck()
-for card in new_deck.all_cards:
-    print(card)
+# Game Setup
+player_one = Player("One")
+player_two = Player("Two")
 
+new_deck = Deck()
 new_deck.shuffle()
-for card in new_deck.all_cards:
-    print(card)
+
+for x in range(26):
+    player_one.add_card(new_deck.deal_one())
+    player_two.add_card(new_deck.deal_one())
+
+game_on = True
+round_num = 0
+
+while game_on:
+    round_num += 1
+    print(f"Round {round_num}")
+
+    if len(player_one.all_cards) == 0:
+        print("Player One, out of cards! Player Two Wins!")
+        game_on = False
+        break
+    if len(player_two.all_cards) == 0:
+        print("Player Two, out of cards! Player One Wins!")
+        game_on = False
+        break
+
+    # Start a new round
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
