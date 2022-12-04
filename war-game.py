b3c377ff6af1234e22db78e7affd2ea0300fc6f9
JenningsF <jenningsf@protonmail.com>
2022-12-03 23:07:32 -0600

    Finished game logic

--- war-game.py
+++ war-game.py
@@ -67,6 +67,8 @@ round_num = 0
 while game_on:
     round_num += 1
     print(f"Round {round_num}")
+    print(player_one)
+    print(player_two)
 
     if len(player_one.all_cards) == 0:
         print("Player One, out of cards! Player Two Wins!")
@@ -83,3 +85,34 @@ while game_on:
 
     player_two_cards = []
     player_two_cards.append(player_two.remove_one())
+
+    at_war = True
+
+    while at_war:
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            player_one.add_card(player_one_cards)
+            player_one.add_card(player_two_cards)
+
+            at_war = False
+        elif player_two_cards[-1].value > player_one_cards[-1].value:
+            player_two.add_card(player_two_cards)
+            player_two.add_card(player_one_cards)
+            
+            at_war = False
+        else:
+            print("WAR!")
+
+            if len(player_one.all_cards) < 3:
+                print("Player One is unable to declare War!")
+                print("Player Two Wins!")
+                game_on = False
+                break
+            elif len(player_two.all_cards) < 3:
+                print("Player Two is unable to declare War!")
+                print("Player One Wins!")
+                game_on = False
+                break
+            else:
+                for num in range(3):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
